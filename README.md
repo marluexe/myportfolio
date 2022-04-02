76d007f8214da45d234c86be2a158f02ee15407c
marlu π <102812167+marluexe@users.noreply.github.com>
2022-04-02 11:40:11 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+Portfólio - Marlu.exe
---
+++ script.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', function() {
+    navBar.classList.toggle('active');
+
+})
+
+navBar.addEventListener('click', function() {
+    navBar.classList.remove('active');
+})
